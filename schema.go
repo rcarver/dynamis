5290e00a14091cf50079a9550b53b9c25f2128e7
Ryan Carver <ryan@ryancarver.com>
2015-09-18 16:57:05 -0700

    schema runners

--- schema.go
+++ schema.go
@@ -1,13 +1,53 @@
 package dynamo
 
-import "github.com/aws/aws-sdk-go/aws"
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+)
 
 // Schema is the interface for managing table schemas.
 type Schema interface {
 
-	// Create ensures that the table(s) exists, to its current definition.
+	// Create ensures that the table(s) exist. It should return an error if
+	// the table already exists or there is any problem creating it.
 	Create(*aws.Config) error
 
-	// Delete ensures that the table(s) do not exist.
+	// Delete ensures that the table(s) do not exist. It should return an
+	// error if the table cannot be deleted or if it does not exist.
 	Delete(*aws.Config) error
+}
+
+// Create makes sure that all of the schemas exist. If abortOnErr is false, it
+// iterates through all schemas even if one returns an error. This is generally
+// what you want to do since each table schema is independent, and it makes
+// calling this function idempotent.
+func Create(cfg *aws.Config, schema []Schema, abortOnErr bool) error {
+	for _, s := range schema {
+		log.Printf("%T Creating...", s)
+		if err := s.Create(cfg); err != nil {
+			log.Printf("%T Error: %s", s, err)
+			if abortOnErr {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// Delete makes sure that none of the schemas exist. If abortOnErr is false, it
+// continues deleting even if one causes an error. This is generally what you
+// want to do, since each table is independent. It also makes this function
+// idempotent.
+func Delete(cfg *aws.Config, schema []Schema, abortOnErr bool) error {
+	for _, s := range schema {
+		log.Printf("%T Deleting...", s)
+		if err := s.Delete(cfg); err != nil {
+			log.Printf("%T Error: %s", s, err)
+			if abortOnErr {
+				return err
+			}
+		}
+	}
+	return nil
 }
---
+++ schema_test.go
@@ -0,0 +1,132 @@
+package dynamo
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+type fakeSchema struct {
+	fail       bool
+	wantCalled bool
+	called     bool
+}
+
+func newFakeSchema(fail bool, wantCall bool) *fakeSchema {
+	return &fakeSchema{fail, wantCall, false}
+}
+
+func (s *fakeSchema) Create(cfg *aws.Config) error {
+	s.called = true
+	if s.fail {
+		return errors.New("failed")
+	}
+	return nil
+}
+func (s *fakeSchema) Delete(cfg *aws.Config) error {
+	s.called = true
+	if s.fail {
+		return errors.New("failed")
+	}
+	return nil
+}
+
+func TestCreate(t *testing.T) {
+	tests := []struct {
+		schemes []Schema
+		abort   bool
+		want    error
+	}{
+		{
+			// All succeed.
+			schemes: []Schema{
+				newFakeSchema(false, true),
+				newFakeSchema(false, true),
+			},
+			abort: true,
+			want:  nil,
+		},
+		{
+			// First fails and abort on error
+			schemes: []Schema{
+				newFakeSchema(true, true),
+				newFakeSchema(false, false),
+			},
+			abort: true,
+			want:  errors.New("failed"),
+		},
+		{
+			// First fails, do not abort.
+			schemes: []Schema{
+				newFakeSchema(true, true),
+				newFakeSchema(false, true),
+			},
+			abort: false,
+			want:  nil,
+		},
+	}
+	for i, test := range tests {
+		got := Create(nil, test.schemes, test.abort)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d Create() got %#v, want %#v", i, got, test.want)
+		}
+		for j, s := range test.schemes {
+			if fs, ok := s.(*fakeSchema); ok {
+				if fs.wantCalled != fs.called {
+					t.Errorf("%d/%d Create called got %#v, want %#v", i, j, fs.called, fs.wantCalled)
+				}
+			}
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tests := []struct {
+		schemes []Schema
+		abort   bool
+		want    error
+	}{
+		{
+			// All succeed.
+			schemes: []Schema{
+				newFakeSchema(false, true),
+				newFakeSchema(false, true),
+			},
+			abort: true,
+			want:  nil,
+		},
+		{
+			// First fails and abort on error
+			schemes: []Schema{
+				newFakeSchema(true, true),
+				newFakeSchema(false, false),
+			},
+			abort: true,
+			want:  errors.New("failed"),
+		},
+		{
+			// First fails, do not abort.
+			schemes: []Schema{
+				newFakeSchema(true, true),
+				newFakeSchema(false, true),
+			},
+			abort: false,
+			want:  nil,
+		},
+	}
+	for i, test := range tests {
+		got := Delete(nil, test.schemes, test.abort)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d Delete() got %#v, want %#v", i, got, test.want)
+		}
+		for j, s := range test.schemes {
+			if fs, ok := s.(*fakeSchema); ok {
+				if fs.wantCalled != fs.called {
+					t.Errorf("%d/%d Delete called got %#v, want %#v", i, j, fs.called, fs.wantCalled)
+				}
+			}
+		}
+	}
+}
